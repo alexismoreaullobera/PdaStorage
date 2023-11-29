17bba0384157fa74942bd74c626997b50472fcf1
alexismoreaulloberauser.email <alexis.moreau.llobera@gmail.com>
2023-11-29 21:34:28 +0100

    feat(PdaStorage)  Implementation StorageContainViewModel + Bindings sur StorageContainView

---
+++ PdaStorage/ViewModels/PdaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PdaStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdaStorage.ViewModels
+{
+    public class PdaViewModel : ViewModelBase
+    {
+        private readonly Pda _pda;
+
+        public int Id => _pda.Id;
+        public string Name => _pda.Name;
+        public int PdaCount => _pda.PdaCount;
+        public int MobCount => _pda.MobCount;
+
+        public PdaViewModel(Pda pda)
+        {
+            _pda = pda;
+        }
+    }
+}
--- PdaStorage/ViewModels/StorageContainViewModel.cs
+++ PdaStorage/ViewModels/StorageContainViewModel.cs
@@ -1,13 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PdaStorage.ViewModels
 {
     public class StorageContainViewModel : ViewModelBase
     {
+        private string _searchedMobName;
+        private readonly ObservableCollection<PdaViewModel> _pdas;
+
+        public ICommand DeleteCommand { get; }
+        public ICommand UpdateCommand { get; }
+        public ICommand ResearchCommand { get; }
+
+        public IEnumerable<PdaViewModel> Pdas => _pdas;
+
+        public string SearchedMobName
+        {
+            get { return _searchedMobName; }
+            set 
+            {
+                if(value == "rechercher un mob..")
+                {
+                    _searchedMobName = "";
+                    OnPropertyChanged(nameof(SearchedMobName));
+                    return;
+                }
+
+                _searchedMobName = value;
+                OnPropertyChanged(nameof(SearchedMobName));
+            }
+        }
+
+        public StorageContainViewModel()
+        {
+            _pdas = new ObservableCollection<PdaViewModel>();
+            _searchedMobName = "rechercher un mob...";
+            _pdas.Add(new PdaViewModel(new Models.Pda("Test1")));
+            _pdas.Add(new PdaViewModel(new Models.Pda("Test2")));
+            _pdas.Add(new PdaViewModel(new Models.Pda("Test3")));
+        }
+
 
     }
 }
